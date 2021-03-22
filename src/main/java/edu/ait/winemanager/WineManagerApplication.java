df8477b73c3bb9e8e4b610e1880d2a1d568bc5ce
RussDHill <russhill@outlook.ie>
2021-03-22 21:19:08 +0000

    Add SpringBootServletInitializer and Configure

--- src/main/java/edu/ait/winemanager/WineManagerApplication.java
+++ src/main/java/edu/ait/winemanager/WineManagerApplication.java
@@ -2,12 +2,18 @@ package edu.ait.winemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WineManagerApplication {
+public class WineManagerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WineManagerApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WineManagerApplication.class);
+	}
 }
